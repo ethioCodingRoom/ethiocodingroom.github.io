c5de04096fcf244dc0270fd447c5bbe93f6ef5a0
ethioCodingRoom <asresgamu@gmail.com>
2026-03-07 21:11:43 +0300

    Add GitHub Actions deployment workflow and update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwind()],
+  base: './', // Add this line here to ensure correct asset loading
+  
 })
